baa6cd6b804e3ae0770df02251d16e5855cb6499
homels <153766918@qq.com>
2020-01-13 23:00:41 +0800

    1. 修复编码规范

--- src/main/java/Contest.java
+++ src/main/java/Contest.java
@@ -2,12 +2,16 @@ public class Contest {
     public static String judge(int number) {
         if (number % 3 == 0) {
             if (number % 5 == 0) {
-                return "fizzbuzz";
+                returnString("fizzbuzz");
             }
-            return "fizz";
+            returnString("fizz");
         } else if (number % 5 == 0) {
-            return "buzz";
+            returnString("buzz");
         }
         return number + "";
+    }
+
+    public static String returnString(String str) {
+        return str;
     }
 }
